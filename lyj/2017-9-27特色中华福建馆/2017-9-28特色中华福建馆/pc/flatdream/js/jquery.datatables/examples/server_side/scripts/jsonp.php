6be27c3813b420bfd86ea49ea4eb0249ba1fbc1e
“your_u <996468104@qq.com>
2017-11-14 11:35:55 +0800

    d

--- lyj/2017-9-27特色中华福建馆/2017-9-28特色中华福建馆/pc/flatdream/js/jquery.datatables/examples/server_side/scripts/jsonp.php
+++
@@ -1,9 +0,0 @@
-<br />
-<b>Warning</b>:  include(/home2/condorth/public_html/datatables/mysql.php): failed to open stream: No such file or directory in <b>/home2/condorth/public_html/flatdream/js/jquery.datatables/examples/server_side/scripts/jsonp.php</b> on line <b>24</b><br />
-<br />
-<b>Warning</b>:  include(): Failed opening '/home2/condorth/public_html/datatables/mysql.php' for inclusion (include_path='.:/opt/php54/lib/php') in <b>/home2/condorth/public_html/flatdream/js/jquery.datatables/examples/server_side/scripts/jsonp.php</b> on line <b>24</b><br />
-<br />
-<b>Warning</b>:  mysql_pconnect(): Access denied for user 'root'@'localhost' (using password: NO) in <b>/home2/condorth/public_html/flatdream/js/jquery.datatables/examples/server_side/scripts/jsonp.php</b> on line <b>45</b><br />
-<br />
-<b>Warning</b>:  Cannot modify header information - headers already sent by (output started at /home2/condorth/public_html/flatdream/js/jquery.datatables/examples/server_side/scripts/jsonp.php:24) in <b>/home2/condorth/public_html/flatdream/js/jquery.datatables/examples/server_side/scripts/jsonp.php</b> on line <b>37</b><br />
-Could not open connection to server
